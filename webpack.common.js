17baa3c4a2b40f985dcbd85826f4e1cc5001d0c4
Omar Moataz <omar.attia@itworx.com>
2020-06-26 14:57:56 +0200

    Moved clean webpack plugin into prod version.

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   mode: "development",
@@ -33,5 +32,5 @@ module.exports = {
   },
   plugins: [new HtmlWebpackPlugin({
     template: "./src/template.html"
-  }), new CleanWebpackPlugin()]
+  })]
 }
